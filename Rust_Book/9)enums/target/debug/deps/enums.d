0118c64e079aef447b8d6373912ba05c833020de
Valve <Gayb>
2023-07-02 05:12:58 +0300

    Работа Rust_book.

---
+++ Rust_Book/9)enums/target/debug/deps/enums.d
@@ -0,0 +1,5 @@
+C:\Users\User\projects-Rust\a\Rust_Book\9)enums\target\debug\deps\enums.exe: src\main.rs
+
+C:\Users\User\projects-Rust\a\Rust_Book\9)enums\target\debug\deps\enums.d: src\main.rs
+
+src\main.rs:
