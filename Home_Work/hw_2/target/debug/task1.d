bffc5e77df7db0e80c74b2be72f832c1d621a66d
Valve <Gayb>
2023-07-04 04:13:18 +0300

    Второе Д/з.

---
+++ Home_Work/hw_2/target/debug/task1.d
@@ -0,0 +1,1 @@
+C:\Users\User\projects-Rust\a\Home_Work\hw_2\target\debug\task1.exe: C:\Users\User\projects-Rust\a\Home_Work\hw_2\src\task1.rs
---
+++ Home_Work/hw_2/target/debug/task2.d
@@ -0,0 +1,1 @@
+C:\Users\User\projects-Rust\a\Home_Work\hw_2\target\debug\task2.exe: C:\Users\User\projects-Rust\a\Home_Work\hw_2\src\task2.rs
